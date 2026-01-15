b03c2c587b09b922269e0afb7eb7ad6ca65f0f85
Rohan <rohanbhatia003@gmail.com>
2026-01-15 23:15:38 +0530

    ..

--- problem1.py
+++ problem1.py
@@ -1,3 +1,17 @@
 class products:
     def __init__(self,name,price):
-        q
+        self.name = name
+        self.price = price
+
+    def info(self):
+        print(f"price of {self.name} is: {self.price}")
+
+        count = 0
+    def count(self):
+        while 
+            
+
+p1 = products("phone",10000)
+p2 = products("lappy" , 100-000) 
+
+p1.info()
